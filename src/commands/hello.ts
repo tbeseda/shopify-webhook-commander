de7fd1031a09e1c77c65acb19b1c331802be3017
Taylor Beseda <tbeseda@gmail.com>
2021-07-07 02:17:06 -0600

    simple file config and better linting/formatting

--- src/commands/hello.ts
+++ src/commands/hello.ts
@@ -1,31 +1,41 @@
-import {Command, flags} from '@oclif/command'
+import { Command, flags } from '@oclif/command';
+import * as fs from 'fs-extra';
+import * as path from 'path';
 
 export default class Hello extends Command {
-  static description = 'describe the command here'
+  static description = 'describe the command here';
 
   static examples = [
     `$ shopify-webhook-commander hello
-hello world from ./src/hello.ts!
+Hello world from ./src/hello.ts!
 `,
-  ]
+  ];
 
   static flags = {
-    help: flags.help({char: 'h'}),
+    help: flags.help({ char: 'h' }),
     // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
+    name: flags.string({ char: 'n', description: 'name to print' }),
     // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
-  }
+    force: flags.boolean({ char: 'f' }),
+  };
 
-  static args = [{name: 'file'}]
+  static args = [{ name: 'file' }];
 
   async run() {
-    const {args, flags} = this.parse(Hello)
+    const { args, flags } = this.parse(Hello);
+    let userConfig;
+    try {
+      userConfig = await fs.readJSON(path.join(this.config.configDir, 'config.json'));
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        this.debug('Missing configuration');
+      }
+    }
 
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from ./src/commands/hello.ts`)
+    const name = flags.name ?? userConfig?.name ?? 'world';
+    this.log(`Hello ${name} from ./src/commands/hello.ts`);
     if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+      this.log(`you input --force and --file: ${args.file}`);
     }
   }
 }
